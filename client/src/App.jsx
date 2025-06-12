fddfca9c7f0f748fb2d3aca8189c315957b47b1d
Sabithf <sabithfariq@gmail.com>
2025-06-12 17:53:21 +0100

    Finalized the fingerprint and Nickname generator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,48 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import QuizPage from './components/QuizPage.jsx';
 import Badge from './components/Badge.jsx';
 import Leaderboard from './components/LeaderBoard.jsx';
-import { getDeviceId } from './utils/fingerprint.js';
+import { getDeviceId, getDeviceIdPaid } from './utils/fingerprint.js';
+import { randomNickName } from './utils/randonNameGenerator.js';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 function App() {
+  const [deviceId, setDeviceId] = useState('');
+  const [nickName, setNickName] = useState('');
 
-  getDeviceId();
+  useEffect(() => {
+    const init = async () => {
+      // Checking the local storage
+      let localDeviceId = localStorage.getItem('deviceId');
+      let localNickName = localStorage.getItem('nickName');
+
+        if (!localDeviceId) {
+          const id = await getDeviceId();
+          if (id){
+            localDeviceId = id;
+            localStorage.setItem('deviceId', id);
+
+          }     
+        }
+
+        if (!localNickName){
+          localNickName = randomNickName;
+          localStorage.setItem('nickName', localNickName);
+        }
+
+      setDeviceId(localDeviceId);
+      setNickName(localNickName);
+
+      console.log(localNickName);
+      console.log(localDeviceId);
+    
+
+  };
+
+    init();})
+
+  
 
   return (
     <Router>
--- client/src/utils/fingerprint.js
+++ client/src/utils/fingerprint.js
@@ -1,20 +1,30 @@
 import FingerprintJS from '@fingerprintjs/fingerprintjs';
 //import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 
-// // Initialize the agent on application start.
-// export const fpPromise = FingerprintJS.load({
-//   apiKey: "fIQ0zvPMwJWeSPFHv0xe",
-//   region: "eu"
-// })
-
-// // Get the visitorId when you need it.
-// fpPromise
-//   .then(fp => fp.get())
-//   .then(result => console.log(result.visitorId))
+// Initialize the agent on application start.
 
 export const getDeviceId =  async () => {
     try {
         const uniqueDevice = await FingerprintJS.load({});
+        const result  = await uniqueDevice.get();
+        console.log("Device ID generated:", result.visitorId);
+        return result.visitorId;
+
+
+    } catch (error) {
+        console.error("Error generating device ID:", error);
+        return null;
+        
+    }
+}
+
+
+export const getDeviceIdPaid =  async () => {
+    try {
+        const uniqueDevice = await FingerprintJS.load({
+            apiKey: "fIQ0zvPMwJWeSPFHv0xe",
+            region: "eu"
+        });
         const result  = await uniqueDevice.get();
         console.log("Device ID generated:", result.visitorId);
         return result.visitorId;
